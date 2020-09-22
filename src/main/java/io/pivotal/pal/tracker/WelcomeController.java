7d7a90e2a390f77e6900a3487b03c544bd578fa5
ravikumardoijad <ravikumar.doijad@atos.net>
2020-09-22 06:43:26 +0000

    changes

---
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -0,0 +1,28 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+
+  //  @Value("${welcome.message}")
+    public String a_welcome_message;
+
+    /*public WelcomeController() {
+
+    }*/
+
+    public WelcomeController(
+            @Value("${welcome.message}")String a_welcome_message
+    ) {
+        this.a_welcome_message = a_welcome_message;
+    }
+
+    @GetMapping("/")
+    public String sayHello() {
+        return  a_welcome_message;
+    }
+}
